08cd969f43c023de2e95229cd1e7fbb2e01b1fa9
Behru <behruz.ravshonbekovich@gmail.com>
2019-10-19 12:54:28 +0500

    some fix

--- query.js
+++ query.js
@@ -3,123 +3,98 @@ var ageBall = 0;
 var operationBall = 0;
 var patientBall = 0;
 
-function calculation() {
-
-  var classList = document.getElementById("result-box").className.split(" ");
-  var arrayAge = document.getElementsByName("age");
-  var checkbox = document.getElementsByName("checkbox-risk");
-
-  checkbox.addEventListener( 'checkbox-risk', function() {
-      if(this.checked) {
-          console.log('chanssssssssssss')
-      } else {
-          console.log('nooooooot')
-      }
-  });
-
-  for (var i = 0; i < arrayAge.length; i++) {
-    if (arrayAge[i].checked) {
-      ageBall = parseInt(arrayAge[i].value);
-    }
-  }
-  var arrayRisk1 = document.getElementsByName("checkbox-risk");
-  for (var i = 0; i < arrayRisk1.length; i++) {
-    if (arrayRisk1[i].checked) {
-      operationBall = operationBall + parseInt(arrayRisk1[i].value)
-    }
-  }
-  var arrayRisk2 = document.getElementsByName("checkbox-risk-2");
-  for (var i = 0; i < arrayRisk2.length; i++) {
-    if (arrayRisk2[i].checked) {
-      patientBall = patientBall + parseInt(arrayRisk2[i].value)
-    }
-  }
-  if (ageBall + operationBall + patientBall === 0 || ageBall + operationBall + patientBall === 1) {
-    classList.forEach(function (classname) {
-      if (classname === "bg-orange") {
-        document.getElementById("result-box").classList.remove("bg-orange");
-      }
-      else if (classname === "bg-light-green") {
-        document.getElementById("result-box").classList.remove("bg-light-green");
-      }
-      else if (classname === "bg-danger") {
-        document.getElementById("result-box").classList.remove("bg-danger");
-      }
-    });
-    document.getElementById("result-box").classList.add("bg-green");
-    document.getElementById("result-box").classList.add("text-white");
-    document.getElementById("result-text").innerText = "Низкий";
-    document.getElementById("conclude").innerHTML = "Специфических мероприятий не требуется; ранняя активизация больного.";
-  } else if (ageBall + operationBall + patientBall === 2) {
-    classList.forEach(function (classname) {
-      if (classname === "bg-green") {
-        document.getElementById("result-box").classList.remove("bg-green");
-      }
-      else if (classname === "bg-orange") {
-        document.getElementById("result-box").classList.remove("bg-orange");
-      }
-      else if (classname === "bg-danger") {
-        document.getElementById("result-box").classList.remove("bg-danger");
-      }
-    });
-    document.getElementById("result-box").classList.add("bg-light-green");
-    document.getElementById("result-box").classList.add("text-white");
-    document.getElementById("result-text").innerText = "Умеренный";
-    document.getElementById("conclude").innerHTML = "Низкие дозы нефракционированного гепарина (каждые 12 ч), низкомолеулярный гепарин (≤3400 МЕ/сут); <br>Чулки с распределением давления или прерывистая пневматическая компрессия.";
-  } else if (ageBall + operationBall + patientBall === 3 || ageBall + operationBall + patientBall === 4) {
-    classList.forEach(function (classname) {
-      if (classname === "bg-green") {
-        document.getElementById("result-box").classList.remove("bg-green");
-      }
-      else if (classname === "bg-light-green") {
-        document.getElementById("result-box").classList.remove("bg-light-green");
-      }
-      else if (classname === "bg-danger") {
-        document.getElementById("result-box").classList.remove("bg-danger");
-      }
-    });
-    document.getElementById("result-box").classList.add("bg-orange");
-    document.getElementById("result-box").classList.add("text-white");
-    document.getElementById("result-text").innerText = "Высокий";
-    document.getElementById("conclude").innerHTML = "Нефракционированный гепарин (каждые 8 ч) или низкомолекулярный гепарин (>3400 МЕ/сут) или перемежающаяся пневматическая компрессия.";
-  } else if (ageBall + operationBall + patientBall >= 5) {
-    classList.forEach(function (classname) {
-      if (classname === "bg-green") {
-        document.getElementById("result-box").classList.remove("bg-green");
-      }
-      else if (classname === "bg-light-green") {
-        document.getElementById("result-box").classList.remove("bg-light-green");
-      }
-      else if (classname === "bg-orange") {
-        document.getElementById("result-box").classList.remove("bg-orange");
-      }
-    });
-    document.getElementById("result-box").classList.add("bg-danger");
-    document.getElementById("result-box").classList.add("text-white");
-    document.getElementById("result-text").innerText = "Очень высокий";
-    document.getElementById("conclude").innerHTML = "Низкомолекулярный гепарин (>3400 МЕ/сут), фондапаринукс, варфарин (МНО 2–3, только в ортопедии) или прерывистая пневматическая компрессия <br> Чулки с распределением давления + нефракционированный гепарин <br> Низкомолекулярный гепарин.";
-  }
-}
 function resetFrom() {
   document.getElementById("form1").reset();
   document.getElementById("form").reset();
+  document.getElementById("form").reset();
 }
 function calculate(input) {
+
     if(input.checked === true){
         if (input.name === "operation-risk") {
-            operationBall = operationBall + input.value;
-        } else if (input.name === "patient-risk") {
-            patientBall = patientBall + input.value;
+            operationBall = operationBall + parseInt(input.value);
+        } else if (input.name === "patient-risk" || input.name === "age") {
+            patientBall = patientBall + parseInt(input.value);
         }
-        all = all + input.value;
-        console.log('add ', input.value)
+        all = all + parseInt(input.value);
+        f();
     }else{
         if (input.name === "operation-risk") {
-            operationBall = operationBall - input.value;
-        } else if (input.name === "patient-risk") {
-            patientBall = patientBall - input.value;
+            operationBall = operationBall - parseInt(input.value);
+        } else if (input.name === "patient-risk" || input.name === "age") {
+            patientBall = patientBall - parseInt(input.value);
         }
-        all = all - input.value;
-        console.log('remove ', input.value)
+        f();
+        all = all - parseInt(input.value);
+    }
+
+}
+
+function f() {
+    var classList = document.getElementById("result-box").className.split(" ");
+    if (ageBall + operationBall + patientBall === 0 || ageBall + operationBall + patientBall === 1) {
+        classList.forEach(function (classname) {
+            if (classname === "bg-orange") {
+                document.getElementById("result-box").classList.remove("bg-orange");
+            }
+            else if (classname === "bg-light-green") {
+                document.getElementById("result-box").classList.remove("bg-light-green");
+            }
+            else if (classname === "bg-danger") {
+                document.getElementById("result-box").classList.remove("bg-danger");
+            }
+        });
+        document.getElementById("result-box").classList.add("bg-green");
+        document.getElementById("result-box").classList.add("text-white");
+        document.getElementById("result-text").innerText = "Низкий";
+        document.getElementById("conclude").innerHTML = "Специфических мероприятий не требуется; ранняя активизация больного.";
+    } else if (ageBall + operationBall + patientBall === 2) {
+        classList.forEach(function (classname) {
+            if (classname === "bg-green") {
+                document.getElementById("result-box").classList.remove("bg-green");
+            }
+            else if (classname === "bg-orange") {
+                document.getElementById("result-box").classList.remove("bg-orange");
+            }
+            else if (classname === "bg-danger") {
+                document.getElementById("result-box").classList.remove("bg-danger");
+            }
+        });
+        document.getElementById("result-box").classList.add("bg-light-green");
+        document.getElementById("result-box").classList.add("text-white");
+        document.getElementById("result-text").innerText = "Умеренный";
+        document.getElementById("conclude").innerHTML = "Низкие дозы нефракционированного гепарина (каждые 12 ч), низкомолеулярный гепарин (≤3400 МЕ/сут); <br>Чулки с распределением давления или прерывистая пневматическая компрессия.";
+    } else if (ageBall + operationBall + patientBall === 3 || ageBall + operationBall + patientBall === 4) {
+        classList.forEach(function (classname) {
+            if (classname === "bg-green") {
+                document.getElementById("result-box").classList.remove("bg-green");
+            }
+            else if (classname === "bg-light-green") {
+                document.getElementById("result-box").classList.remove("bg-light-green");
+            }
+            else if (classname === "bg-danger") {
+                document.getElementById("result-box").classList.remove("bg-danger");
+            }
+        });
+        document.getElementById("result-box").classList.add("bg-orange");
+        document.getElementById("result-box").classList.add("text-white");
+        document.getElementById("result-text").innerText = "Высокий";
+        document.getElementById("conclude").innerHTML = "Нефракционированный гепарин (каждые 8 ч) или низкомолекулярный гепарин (>3400 МЕ/сут) или перемежающаяся пневматическая компрессия.";
+    } else if (ageBall + operationBall + patientBall >= 5) {
+        classList.forEach(function (classname) {
+            if (classname === "bg-green") {
+                document.getElementById("result-box").classList.remove("bg-green");
+            }
+            else if (classname === "bg-light-green") {
+                document.getElementById("result-box").classList.remove("bg-light-green");
+            }
+            else if (classname === "bg-orange") {
+                document.getElementById("result-box").classList.remove("bg-orange");
+            }
+        });
+        document.getElementById("result-box").classList.add("bg-danger");
+        document.getElementById("result-box").classList.add("text-white");
+        document.getElementById("result-text").innerText = "Очень высокий";
+        document.getElementById("conclude").innerHTML = "Низкомолекулярный гепарин (>3400 МЕ/сут), фондапаринукс, варфарин (МНО 2–3, только в ортопедии) или прерывистая пневматическая компрессия <br> Чулки с распределением давления + нефракционированный гепарин <br> Низкомолекулярный гепарин.";
     }
 }
